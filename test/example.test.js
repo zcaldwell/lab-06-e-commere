37b6fc8bc04076b9567d13961d2addc80f6c688a
zcaldwell <mintyst.john@gmail.com>
2021-10-11 17:26:29 -0700

    css and testeing put in

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,19 @@
 // IMPORT MODULES under test here:
+import { renderCars } from "../render-cars.js";
+import { cars } from "../things.js";
 // import { example } from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('rendarCars should return HTML', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="car-listing"><h2>Saturn</h2><h3>S-Series</h3><img src="./assets/saturn.jpg"></div>`;
+    const saturn = cars[0];
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderCars(saturn).outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
